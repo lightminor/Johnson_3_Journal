4d4e3c6f5a7837f14bcc54e59d7ab1bd9131c767
D22 <LightMajor@ninja12.d22.lan>
2015-09-21 06:05:19 -0500

    Notes

---
+++ Notes/Johnson_3_Datatypes_Notes.java
@@ -0,0 +1,52 @@
+/*Data Types
+ *int - integer 
+ *short, long double - other decimals
+ *char - characters "e"
+ *string - multiple characters
+ *boolean - True / False Statements
+ *flood - Flooding point decimal
+*/
+package johnson_3_datatypes_notes;
+
+/**
+ *
+ * @author LightMajor
+ */
+public class Johnson_3_Datatypes_Notes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 0;
+        float a = 0.01f;
+        double b = 0.01;        
+        short c = 123;
+        char d = 123;
+        String e = "Words";
+        
+        //here we will look at some mathmatical operators
+        // + this adds two or more numbers together
+        // - this subtracts two or more numbers
+        // * this multiplies two or more numbers
+        // / this divides two or more numbers
+        // % this is a module . it shows the remiander 
+        
+        System.out.println(i + "an integer");
+        System.out.println("a float");
+        System.out.println("a double");
+        System.out.println("a short");
+        System.out.println("a char");
+        System.out.println("a string");
+        System.out.println("a boolean");
+        for(i=0; i < 200; i++){
+           System.out.println("Things");
+        }
+        
+        
+        
+        
+    }
+    
+}
